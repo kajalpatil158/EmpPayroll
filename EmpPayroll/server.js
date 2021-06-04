f2530cf64082c432aad610e08383a6d33fe68ae8
Kajal <kajalpatil158@gmail.com>
2021-06-04 17:08:24 +0530

    feat : Chai Testing Code Added In Server

--- EmpPayroll/server.js
+++ EmpPayroll/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 require('dotenv').config();
+const morgan = require('morgan');
 
 //const PORT = process.env.PORT || 7000;
 
@@ -15,6 +16,12 @@ app.use(express.urlencoded({ extended: true }))
 
 // parse requests 
 app.use(express.json())
+
+//don't show the log when it is test
+if (config.util.getEnv('NODE_ENV') !== 'test') {
+    //use morgan to log at command line
+    app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+}
 
 // define a simple route
 app.get('/', (req, res) => {
