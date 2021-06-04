45c293bd0d87b89ffdbdb22d244fe4bd76bd3bb5
Kajal <kajalpatil158@gmail.com>
2021-06-04 22:46:29 +0530

    Refactore : Swagger Code

--- EmpPayroll/server.js
+++ EmpPayroll/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-// create express app
-const app = express();
+const morgan = require('morgan');
+const swaggerAutogen = require('swagger-autogen')();
+
+const swaggerAutogen: (outputFile: < string > , endpointsFiles: < Array of string > , data: < object > ) => Promise < any >
+    // create express app
+    const app = express();
 
 require('dotenv').config();
-const morgan = require('morgan');
+
 
 //const PORT = process.env.PORT || 7000;
 
@@ -18,7 +22,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 //don't show the log when it is test
-if (config.util.getEnv('NODE_ENV') !== 'test') {
+if (URL !== 'test') {
     //use morgan to log at command line
     app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
 }
