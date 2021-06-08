dc11e3b1cd27f0a0db805e43218f00eb6ba7d0f4
Kajal <kajalpatil158@gmail.com>
2021-06-08 10:45:26 +0530

    feat ; empdata json file is added for unit testing

--- EmpPayroll/test/emp.test.js
+++ EmpPayroll/test/emp.test.js
@@ -4,15 +4,19 @@ const chaiHttp = require("chai-http");
 chai.should();
 chai.use(chaiHttp);
 
+const fs = require('fs');
+let data = fs.readFileSync('test/empdata.json');
+let emptest = JSON.parse(data);
+
 describe('POST/login', () => {
-    it('It should POST new task', (done) => {
-        const task = {
+    it('Post Registration', (done) => {
+        const Registration = {
             "emailId": "rutu@gmail.com",
             "password": "jklaa12"
         };
         chai.request(server)
             .post('/login')
-            .send(task)
+            .send(Registration)
             .end((error, res) => {
                 res.should.have.status(200);
                 res.body.should.be.property('success').eq(true);
